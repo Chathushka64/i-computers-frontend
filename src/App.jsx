d3e59e6062249f519843e237c5f93532ea435350
Chathushka64 <lapex852@gmail.com>
2026-05-04 19:55:16 +0530

    Add react-router-dom dependency and update App component layout ✏️

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,8 @@ import ProductCard from './components/productCard'
 function App() {
 
   return (
-    <div className='w-[700px] h-[700px] bg-yellow-300'>
+    <div className='w-full h-screen bg-red-900'>
 
-      <div className='w-[600px] h-[600px] bg-gray-400 '>
-
-        <div className='w-[100px] h-[100px] bg-blue-700'></div>
-        <div className='w-[100px] h-[100px] bg-red-700'></div>
-        <div className='w-[100px] h-[100px] bg-white'></div>
-        <div className='w-[100px] h-[100px] bg-green-700'></div>
-        <div className='w-[100px] h-[100px] bg-black'></div>
-      </div>
     </div>
   )
 }
